99addf3519ba882cd13ebb04bebd7a07fe180550
Julian Becker <julian.becker@isb-ik.de>
2022-03-07 10:38:51 +0100

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+const increment = (x: number) => ++x;
+const resource = (x: number) => `../assets/Minutengeschichte${x}.mp3`;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'glueckspilzzauber';
+  title = 'Glückspilzzauber';
+  sources = [...Array(10).keys()].map(increment).map(resource);
 }
